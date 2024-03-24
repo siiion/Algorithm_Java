5aec1946192e9208a2851cc55106df6448d68c03
전시원 <112245253+siiion@users.noreply.github.com>
2024-03-24 15:18:32 +0900

    [level 0] Title: 암호 해독, Time: 1.25 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,12 +1,11 @@
 class Solution {
     public String solution(String cipher, int code) {
         String answer = "";
-        String[] array = cipher.split("");
-        for (int i = 0; i < array.length; i++) {
-            if ((i + 1) % code == 0) {
-                answer += array[i];
-            }
+
+        for (int i = code; i <= cipher.length(); i = i + code) {
+            answer += cipher.substring(i - 1, i);
         }
+
         return answer;
     }
 }
