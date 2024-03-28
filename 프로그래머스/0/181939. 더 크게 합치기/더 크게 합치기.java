942572b2e3ab7cb4d43444929d6a085a07a599bc
전시원 <112245253+siiion@users.noreply.github.com>
2024-03-28 15:07:09 +0900

    [level 0] Title: 더 크게 합치기, Time: 8.61 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int a, int b) {
         String str1 = (a + "") + (b + "");
         String str2 = (b + "") + (a + "");
-        int answer = Integer.valueOf(str1) >= Integer.valueOf(str2) ? Integer.valueOf(str1) : Integer.valueOf(str2);
+        int answer = Integer.parseInt(str1) >= Integer.parseInt(str2) ? Integer.parseInt(str1) : Integer.parseInt(str2);
         return answer;
     }
 }
