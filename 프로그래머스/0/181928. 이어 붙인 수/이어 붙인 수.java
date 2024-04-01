50c70886279d2d8c67401986a5a0c91c3b1c3cce
전시원 <112245253+siiion@users.noreply.github.com>
2024-04-01 15:22:09 +0900

    [level 0] Title: 이어 붙인 수, Time: 11.22 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list) {
+        String odd = "";
+        String even = "";
+        for (int i : num_list) {
+            if (i % 2 == 0) {
+                even += i + "";
+            } else {
+                odd += i + "";
+            }
+        }
+        return Integer.parseInt(odd) + Integer.parseInt(even);
+    }
+}
