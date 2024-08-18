9f2c47de61ab04b671062ffcb7cff8cc6b19df74
전시원 <112245253+siiion@users.noreply.github.com>
2024-08-18 13:10:16 +0900

    [level 0] Title: 배열 만들기 2, Time: 57.76 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int l, int r) {
+        List<Integer> arrList = new ArrayList<>();
+        boolean isCorrect = false;
+        boolean isContained = false;
+        for (int i = l; i <= r; i++) {
+            String s = Integer.toString(i);
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == '0' || s.charAt(j) == '5') {
+                    isCorrect = true;
+                    continue;
+                } else {
+                    isCorrect = false;
+                    break;
+                }
+            }
+            if (isCorrect) {
+                int num = Integer.parseInt(s);
+                arrList.add(num);
+                isContained = true;
+            }
+        }
+        if (arrList.size() == 0) {
+            int[] answer = {-1};
+            return answer;
+        } else {
+            int[] answer = new int[arrList.size()];
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = arrList.get(i);
+            }
+            return answer;
+        }
+    }
+}
