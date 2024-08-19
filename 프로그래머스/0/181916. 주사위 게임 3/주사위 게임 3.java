7b1fa5bdf85ebc6919605ba1abd55a05e8941b08
전시원 <112245253+siiion@users.noreply.github.com>
2024-08-19 16:30:33 +0900

    [level 0] Title: 주사위 게임 3, Time: 1.58 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+        int answer = 0;
+        
+        int[] arr = {a, b, c, d};
+        Arrays.sort(arr);
+        
+        if (arr[0] == arr[3]) {  
+            answer = 1111 * arr[0];
+        } else if (arr[0] == arr[2] || arr[1] == arr[3]) {  
+            if (arr[0] == arr[2]) {
+                answer = (int)Math.pow(10 * arr[0] + arr[3], 2);
+            } else {
+                answer = (int)Math.pow(10 * arr[1] + arr[0], 2);
+            }
+        } else if (arr[0] == arr[1] || arr[1] == arr[2] || arr[2] == arr[3]) { 
+            if (arr[0] == arr[1] && arr[2] == arr[3]) {
+                answer = (arr[0] + arr[2]) * (arr[2] - arr[0]);
+            } else if (arr[0] == arr[1] && arr[2] != arr[3]) {
+                answer = arr[2] * arr[3];
+            } else if (arr[1] == arr[2]) {
+                answer = arr[0] * arr[3];
+            } else {
+                answer = arr[0] * arr[1];
+            }
+        } else {
+            answer = arr[0];
+        }
+        return answer;
+    }
+}
