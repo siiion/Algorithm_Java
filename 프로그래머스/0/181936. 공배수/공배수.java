26d7266e0b93060754c6242d4eb78eef30f6eacc
전시원 <112245253+siiion@users.noreply.github.com>
2025-03-05 13:04:38 +0900

    [level 0] Title: 공배수, Time: 0.04 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.java
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int number, int n, int m) {
-        int answer = number % n == 0 && number % m == 0 ? 1 : 0;
-        return answer;
+        return (number % n == 0) && (number % m == 0) ? 1 : 0;
     }
 }
