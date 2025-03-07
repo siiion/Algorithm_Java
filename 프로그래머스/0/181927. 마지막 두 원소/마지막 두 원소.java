c54ce3503b37ab25d47eb4079c2b5f6208e8f42e
전시원 <112245253+siiion@users.noreply.github.com>
2025-03-07 09:20:39 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -3,13 +3,15 @@ class Solution {
         int[] answer = new int[num_list.length + 1];
         int num1 = num_list[num_list.length - 1];
         int num2 = num_list[num_list.length - 2];
+        
         for (int i = 0; i < num_list.length; i++) {
             answer[i] = num_list[i];
         }
+        
         if (num1 > num2) {
-            answer[answer.length - 1] = num1 - num2;
+            answer[num_list.length] = num1 - num2;
         } else {
-            answer[answer.length - 1] = num1 * 2;
+            answer[num_list.length] = num1 * 2;
         }
         return answer;
     }
