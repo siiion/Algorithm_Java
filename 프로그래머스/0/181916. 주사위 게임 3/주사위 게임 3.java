fb2be1224998291a03a8ae7a092ddced7507c47b
전시원 <112245253+siiion@users.noreply.github.com>
2025-03-10 13:46:30 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.92 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int a, int b, int c, int d) {
@@ -7,18 +7,18 @@ class Solution {
         int[] arr = {a, b, c, d};
         Arrays.sort(arr);
         
-        if (arr[0] == arr[3]) {  
+        if (arr[0] == arr[3]) {
             answer = 1111 * arr[0];
-        } else if (arr[0] == arr[2] || arr[1] == arr[3]) {  
+        } else if (arr[0] == arr[2] || arr[1] == arr[3]) {
             if (arr[0] == arr[2]) {
-                answer = (int)Math.pow(10 * arr[0] + arr[3], 2);
+                answer = (int) Math.pow(10 * arr[0] + arr[3], 2);
             } else {
-                answer = (int)Math.pow(10 * arr[1] + arr[0], 2);
+                answer = (int) Math.pow(10 * arr[3] + arr[0], 2);
             }
-        } else if (arr[0] == arr[1] || arr[1] == arr[2] || arr[2] == arr[3]) { 
-            if (arr[0] == arr[1] && arr[2] == arr[3]) {
-                answer = (arr[0] + arr[2]) * (arr[2] - arr[0]);
-            } else if (arr[0] == arr[1] && arr[2] != arr[3]) {
+        } else if (arr[0] == arr[1] && arr[2] == arr[3]) {
+            answer = (arr[0] + arr[2]) * (arr[2] - arr[0]);
+        } else if (arr[0] == arr[1] || arr[1] == arr[2] || arr[2] == arr[3]) {
+            if (arr[0] == arr[1]) {
                 answer = arr[2] * arr[3];
             } else if (arr[1] == arr[2]) {
                 answer = arr[0] * arr[3];
