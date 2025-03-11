5b43e47fb1c05a3abd5f44cd0a4a555b71279a24
전시원 <112245253+siiion@users.noreply.github.com>
2025-03-11 20:20:46 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.06 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,9 +1,17 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        if (my_string.endsWith(is_suffix)) {
-            return 1;
+        int answer = 0;
+        
+        if (is_suffix.length() >= my_string.length()) {
+            if (is_suffix.equals(my_string)) {
+                answer = 1;
+            }
+        } else if (my_string.substring(my_string.length() - is_suffix.length()).equals(is_suffix)) {
+            answer = 1;
         } else {
-            return 0;
+            answer = 0;
         }
+        
+        return answer;
     }
 }
