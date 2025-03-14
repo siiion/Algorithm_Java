56409ae9dd60a98b3fde9a0776f023c783456127
전시원 <112245253+siiion@users.noreply.github.com>
2025-03-14 15:41:04 +0900

    [level 0] Title: 글자 지우기, Time: 2.40 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,13 +1,15 @@
 class Solution {
     public String solution(String my_string, int[] indices) {
-        String[] strArr = my_string.split("");
+        String answer = "";
+        String[] arr = my_string.split("");
         
-        for (int i = 0; i < indices.length; i++) {
-            int index = indices[i];
-            strArr[index] = "";
+        for (int i : indices) {
+            arr[i] = "";
         }
         
-        String answer = String.join("", strArr);
+        for (String str : arr) {
+            answer += str;
+        }
         
         return answer;
     }
