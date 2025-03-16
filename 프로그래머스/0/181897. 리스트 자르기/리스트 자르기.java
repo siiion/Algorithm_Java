8ba2f44b809bff968f50dce54acfdd9393ad1be5
전시원 <112245253+siiion@users.noreply.github.com>
2025-03-16 20:10:27 +0900

    [level 0] Title: 리스트 자르기, Time: 5.28 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,35 +1,36 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        ArrayList<Integer> arrList = new ArrayList<>();
+        List<Integer> arrList = new ArrayList<>();
         int a = slicer[0];
         int b = slicer[1];
         int c = slicer[2];
         
-        if (n == 1) {
-            for (int i = 0; i <= b; i++) {
-                arrList.add(num_list[i]);
-            }
-        } else if (n == 2) {
-            for (int i = a; i < num_list.length; i++) {
-                arrList.add(num_list[i]);
-            }
-        } else if (n == 3) {
-            for (int i = a; i <= b; i++) {
-                arrList.add(num_list[i]);
-            }
-        } else {
-            for (int i = a; i <= b; i += c) {
-                arrList.add(num_list[i]);
-            }
+        switch (n) {
+            case 1:
+                for (int i = 0; i <= b; i++) {
+                    arrList.add(num_list[i]);
+                }
+                break;
+            case 2:
+                for (int i = a; i < num_list.length; i++) {
+                    arrList.add(num_list[i]);
+                }
+                break;
+            case 3:
+                for (int i = a; i <= b; i++) {
+                    arrList.add(num_list[i]);
+                }
+                break;
+            default:
+                for (int i = a; i <= b; i += c) {
+                    arrList.add(num_list[i]);
+                }
+                break;
         }
         
-        int[] answer = new int[arrList.size()];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = arrList.get(i);
-        }
-        
+        int[] answer = arrList.stream().mapToInt(i->i).toArray();
         return answer;
     }
 }
