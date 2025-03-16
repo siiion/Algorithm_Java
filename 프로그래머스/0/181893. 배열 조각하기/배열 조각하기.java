d80bd65e79cca5ec92f84ef82665890ce41c7b6c
전시원 <112245253+siiion@users.noreply.github.com>
2025-03-16 20:45:28 +0900

    [level 0] Title: 배열 조각하기, Time: 1.00 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -1,10 +1,10 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int[] query) {
         for (int i = 0; i < query.length; i++) {
             if (i % 2 == 0) {
-                arr = Arrays.copyOf(arr, query[i] + 1);
+                arr = Arrays.copyOfRange(arr, 0, query[i] + 1);
             } else {
                 arr = Arrays.copyOfRange(arr, query[i], arr.length);
             }
