fcb7e359440bed47305a15f5f86ad4b39c8290d3
전시원 <112245253+siiion@users.noreply.github.com>
2025-03-17 13:27:19 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 2.91 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -1,19 +1,31 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public String[] solution(String[] str_list) {
         String[] answer = {};
+        int index = -1;
+        String direction = "";
         
         for (int i = 0; i < str_list.length; i++) {
             if (str_list[i].equals("l")) {
-                answer = Arrays.copyOf(str_list, i);
+                direction += "l";
+                index = i;
                 break;
             } else if (str_list[i].equals("r")) {
-                answer = Arrays.copyOfRange(str_list, i + 1, str_list.length);
+               direction += "r";
+                index = i;
                 break;
             }
         }
         
-        return answer;
+        if (index != -1) {
+            if (direction.equals("l")) {
+                 return Arrays.copyOfRange(str_list, 0, index);
+            } else {
+                return Arrays.copyOfRange(str_list, index + 1, str_list.length);
+            }
+        } else {
+            return answer;
+        }
     }
 }
