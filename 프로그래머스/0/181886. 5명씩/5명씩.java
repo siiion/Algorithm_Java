2165afd7d98b8963808e3f90c92a2e35fbf5039c
전시원 <112245253+siiion@users.noreply.github.com>
2025-03-18 11:14:09 +0900

    [level 0] Title: 5명씩, Time: 0.04 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,9 +1,19 @@
+import java.util.*;
+
 class Solution {
     public String[] solution(String[] names) {
-        String[] answer = names.length % 5 == 0 ? new String[names.length / 5] : new String[names.length / 5 + 1];
+        List<String> arrList = new ArrayList<>();
+        arrList.add(names[0]);
         
+        if (names.length >= 5) {
+            for (int i = 5; i < names.length; i+= 5) {
+            arrList.add(names[i]);
+        }
+        }
+        
+        String[] answer = new String[arrList.size()];
         for (int i = 0; i < answer.length; i++) {
-            answer[i] = names[i * 5];
+            answer[i] = arrList.get(i);
         }
         
         return answer;
