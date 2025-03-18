00cd1b3cad8ec2a8827e2bdd0c01bf7dac8e22c1
전시원 <112245253+siiion@users.noreply.github.com>
2025-03-18 11:27:41 +0900

    [level 0] Title: 할 일 목록, Time: 0.04 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -1,18 +1,17 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        ArrayList<String> arrList = new ArrayList<>();
+        List<String> arrList = new ArrayList<>();
         
-        for (int i = 0; i < todo_list.length; i++) {
+        for (int i = 0; i < finished.length; i++) {
             if (!finished[i]) {
                 arrList.add(todo_list[i]);
             }
         }
         
-        String[] answer = new String[arrList.size()];
-        
-        for (int i = 0; i < answer.length; i++) {
+        String[] answer =  new String[arrList.size()];
+        for (int i = 0; i < arrList.size(); i++) {
             answer[i] = arrList.get(i);
         }
         
