4f1bad1324b66908a6d0f9e02a1205ca78534111
전시원 <112245253+siiion@users.noreply.github.com>
2025-03-20 20:28:43 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 12.16 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -1,5 +1,11 @@
 class Solution {
     public String solution(String myString) {
-        return myString.toUpperCase();
+        String answer = "";
+        
+        for (char c : myString.toCharArray()) {
+            answer += Character.toUpperCase(c);
+        }
+        
+        return answer;
     }
 }
