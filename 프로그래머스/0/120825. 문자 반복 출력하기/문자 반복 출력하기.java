6b2986c3049d6297d9fd54c65840349545baf3ef
전시원 <112245253+siiion@users.noreply.github.com>
2025-04-07 13:33:02 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 1.16 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        
+        String[] arr = my_string.split("");
+        
+        for (String s : arr) {
+            answer += s.repeat(n);
+        }
+        
+        return answer;
+    }
+}
