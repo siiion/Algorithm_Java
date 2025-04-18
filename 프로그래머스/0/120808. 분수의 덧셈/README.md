38497b90eca3ee8f5b82bc3b77ef9184e3088ece
전시원 <112245253+siiion@users.noreply.github.com>
2025-04-18 15:50:31 +0900

    [level 0] Title: 분수의 덧셈, Time: 4.36 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/README.md
+++ 프로그래머스/0/120808. 분수의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 0] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808?language=java) 
 
 ### 성능 요약
 
-메모리: 90.7 MB, 시간: 7.59 ms
+메모리: 77 MB, 시간: 4.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 02일 11:33:11
+2025년 04월 18일 15:50:23
 
 ### 문제 설명
 
--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,18 +1,35 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = new int[2];
-        int denom = denom1 * denom2;
-        int numer = numer1 * denom2 + numer2 * denom1;
-        int max = 1;
+        int numer = 1;
+        int denom = 1;
         
-        for (int i = 1; i <= numer && i <= denom; i++) {
-            if (numer % i == 0 && denom % i == 0) {
-                max = i;
+        numer = numer1 * denom2 + numer2 * denom1;
+        denom = denom1 * denom2;
+        
+        if (numer > denom) {
+            for (int i = denom; i > 1; i--) {
+                if ((numer % i == 0) && (denom % i == 0)) {
+                    numer /= i;
+                    denom /= i;
+                    i = denom;
+                }
+            }
+        } else if (numer == denom) {
+            numer = 1;
+            denom = 1;
+        } else {
+            for (int i = numer; i > 1; i--) {
+                if ((numer % i == 0) && (denom % i == 0)) {
+                    numer /= i;
+                    denom /= i;
+                    i = numer;
+                }
             }
         }
         
-        answer[0] = numer / max;
-        answer[1] = denom / max;
+        answer[0] = numer;
+        answer[1] = denom;
         
         return answer;
     }
