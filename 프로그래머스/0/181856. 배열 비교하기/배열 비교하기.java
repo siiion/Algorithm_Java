7c462c7ea73985a922c39b2e759443021db031a7
전시원 <112245253+siiion@users.noreply.github.com>
2025-07-04 11:51:28 +0900

    [level 0] Title: 배열 비교하기, Time: 1.92 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -1,20 +1,13 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
-        int sum1 = 0;
-        int sum2 = 0;
+        int answer = Integer.compare(arr1.length, arr2.length);
         
-        if (arr1.length == arr2.length) {
-            for (int i : arr1) {
-                sum1 += i;
-            }
-            for (int j : arr2) {
-                sum2 += j;
-            }
-            
-            if (sum1 == sum2) return 0;
-            return sum1 > sum2 ? 1 : -1;
-        } else {
-            return arr1.length > arr2.length ? 1 : -1;
+        if (answer == 0) {
+            answer = Integer.compare(IntStream.of(arr1).sum(), IntStream.of(arr2).sum());
         }
+        
+        return answer;
     }
 }
