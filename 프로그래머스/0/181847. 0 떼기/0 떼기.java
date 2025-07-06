93a8acbe4163fd5b5a1f1befca6692579f20dc78
전시원 <112245253+siiion@users.noreply.github.com>
2025-07-06 19:14:35 +0900

    [level 0] Title: 0 떼기, Time: 0.10 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,6 +1,5 @@
 class Solution {
     public String solution(String n_str) {
-        int i = Integer.parseInt(n_str);
-        return Integer.toString(i);
+        return n_str.replaceFirst("^0+", "");
     }
 }
