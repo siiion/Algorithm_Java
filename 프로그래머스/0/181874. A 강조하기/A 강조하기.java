100a128119a128b618a36cf6f4d6f808087a762a
전시원 <112245253+siiion@users.noreply.github.com>
2025-07-07 23:59:53 +0900

    [level 0] Title: A 강조하기, Time: 10.52 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -2,11 +2,15 @@ class Solution {
     public String solution(String myString) {
         String answer = "";
         
-        for (char c : myString.toCharArray()) {
-            if (c == 'a' || c == 'A') {
-                answer += 'A';
+        for (int i = 0; i < myString.length(); i++) {
+            char ch = myString.charAt(i);
+            
+            if (ch == 'a') {
+                answer += "A";
+            } else if (ch > 'A' && ch < 'a') {
+                answer += Character.toLowerCase(ch);
             } else {
-                answer += Character.toLowerCase(c);
+                answer += ch;
             }
         }
         
