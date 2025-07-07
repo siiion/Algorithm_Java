30942f8f2f118031cf5c7810e5c64066f5f2d7d3
전시원 <112245253+siiion@users.noreply.github.com>
2025-07-07 23:35:07 +0900

    [level 0] Title: 배열 만들기 1, Time: 9.00 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,16 +1,19 @@
-import java.util.*;
+import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int n, int k) {
-        List<Integer> arrList = new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>();
         
         for (int i = 1; i <= n; i++) {
             if (i % k == 0) {
-                arrList.add(i);
+                result.add(i);
             }
         }
         
-        int[] answer = arrList.stream().mapToInt(i->i).toArray();
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
         return answer;
     }
 }
