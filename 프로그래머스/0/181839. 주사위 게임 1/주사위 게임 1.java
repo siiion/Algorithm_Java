f333ab4ffc5295487e7eafd12beccc9f6d40d5c0
전시원 <112245253+siiion@users.noreply.github.com>
2025-07-07 21:14:47 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -1,18 +1,11 @@
 class Solution {
     public int solution(int a, int b) {
-        int answer = 0;
-        
-        if (a % 2 == 0 & b % 2 == 0) {
-            if (a > b) {
-                answer = a - b;
-            } else {
-                answer = b - a;
-            }
-        } else if (a % 2 == 0 || b % 2 == 0) {
-            answer = 2 * (a + b);
+        if (a % 2 != 0 && b % 2 != 0) {
+            return a * a + b * b;
+        } else if (a % 2 != 0 || b % 2 != 0) {
+            return 2 * (a + b);
         } else {
-            answer = a * a + b * b;
+            return a - b > 0 ? a - b : - (a - b);
         }
-        return answer;
     }
 }
