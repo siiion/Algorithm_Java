8bddfb54e94b0b14fc425c7078a08f8e0db4d8ed
전시원 <112245253+siiion@users.noreply.github.com>
2025-07-12 17:22:35 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.69 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -1,14 +1,10 @@
 class Solution {
     public int solution(int[][] arr) {
-        int n = arr.length;
-        boolean flag = true;
+        Boolean flag = true;
         
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (arr[i][j] != arr[j][i]) {
-                    flag = false;
-                    break;
-                }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] != arr[j][i]) flag = false;
             }
         }
         
