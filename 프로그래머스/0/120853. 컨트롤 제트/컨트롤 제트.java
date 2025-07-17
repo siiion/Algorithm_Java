1f08769df52b2da6276d403ceb0367db7cf93687
전시원 <112245253+siiion@users.noreply.github.com>
2025-07-17 15:47:42 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.13 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] arr = s.split(" ");
+        
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (!arr[i + 1].equals("Z") && !arr[i].equals("Z")) {
+                answer += Integer.parseInt(arr[i]);
+            }
+        }
+        
+        if (!arr[arr.length - 1].equals("Z")) {
+            answer += Integer.parseInt(arr[arr.length - 1]);
+        }
+        
+        return answer;
+    }
+}
