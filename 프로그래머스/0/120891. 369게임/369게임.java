afb8d31610dd25e677ee3944aaeab34dd75bb654
전시원 <112245253+siiion@users.noreply.github.com>
2025-07-23 15:28:12 +0900

    [level 0] Title: 369게임, Time: 7.66 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String str = order + "";
+        
+        for (char c : str.toCharArray()) {
+            if (c == '3' || c == '6' || c == '9') {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
