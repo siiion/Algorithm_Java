d9d521617881c8ade5d00b0c693c97c6f78b5daa
전시원 <112245253+siiion@users.noreply.github.com>
2025-07-24 11:51:03 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 11.38 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        char[] arr = my_string.toCharArray();
+        
+        char temp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = temp;
+        
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+        
+        return answer;
+    }
+}
