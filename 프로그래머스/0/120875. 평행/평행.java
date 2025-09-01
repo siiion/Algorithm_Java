c93cb05bcddffdec25e3da7f82e901b0013456a3
전시원 <112245253+siiion@users.noreply.github.com>
2025-09-01 12:53:58 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[][] dots) {
+        if (isParallel(dots[0], dots[1], dots[2], dots[3])) return 1;
+        if (isParallel(dots[0], dots[2], dots[1], dots[3])) return 1;
+        if (isParallel(dots[0], dots[3], dots[1], dots[2])) return 1;
+        return 0;
+    }
+    
+    private boolean isParallel(int[] a, int[] b, int[] c, int[] d) {
+        long dx1 = b[0] - a[0];
+        long dy1 = b[1] - a[1];
+        long dx2 = d[0] - c[0];
+        long dy2 = d[1] - c[1];
+        return dy1 * dx2 == dy2 * dx1;
+    }
+}
