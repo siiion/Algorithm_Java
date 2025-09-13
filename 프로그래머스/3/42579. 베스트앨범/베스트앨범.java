1ed7a2f3a0b0e99549099d6896761e81af8bb357
전시원 <112245253+siiion@users.noreply.github.com>
2025-09-13 14:22:23 +0900

    [level 3] Title: 베스트앨범, Time: 4.29 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -14,6 +14,7 @@ class Solution {
         Map<String, Integer> genreTotal = new HashMap<>();
         Map<String, List<Track>> genreTracks = new HashMap<>();
         
+        // 장르 별 총 재생 횟수
         for (int i = 0; i < genres.length; i++) {
             String g = genres[i];
             int p = plays[i];
@@ -22,9 +23,11 @@ class Solution {
                 .add(new Track(i, p));
         }
         
+        // 장르 재생 횟수에 따라 정렬하기
         List<String> orderedGenres = new ArrayList<>(genreTotal.keySet());
         orderedGenres.sort((a, b) -> genreTotal.get(b) - genreTotal.get(a));
         
+        // 장르 내에서 재생 횟수, 고유 번호에 따라 트랙(노래) 정렬하기
         for (List<Track> list : genreTracks.values()) {
             list.sort((t1, t2) -> {
                 if (t1.plays != t2.plays) return t2.plays - t1.plays;
@@ -32,6 +35,7 @@ class Solution {
             });
         }
         
+        // 장르별로 최대 2개의 노래씩 
         List<Integer> result = new ArrayList<>();
         for (String g : orderedGenres) {
             List<Track> list = genreTracks.get(g);
