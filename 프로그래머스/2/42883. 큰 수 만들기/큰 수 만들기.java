363a26870164a2a0d221f5f1c7732628a0af4dac
전시원 <112245253+siiion@users.noreply.github.com>
2025-10-05 15:28:42 +0900

    [level 2] Title: 큰 수 만들기, Time: 65.71 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < number.length(); i++) {
+            char c = number.charAt(i);
+            while (k > 0 && sb.length() > 0 && sb.charAt(sb.length() - 1) < c) {
+                sb.deleteCharAt(sb.length() - 1);
+                k--;
+            }
+            sb.append(c);
+        }
+        
+        if (k > 0) sb.setLength(sb.length() - k);
+        return sb.toString();
+    }
+}
